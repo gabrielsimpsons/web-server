e9135b5f6042d4309cf7043df80bcd5a37c788c2
Constant Concept <gabrielsimpsons@outlook.com>
2017-09-14 19:14:25 -0500

    Inicio proyecto

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const hbs = require('hbs')
+const fs = require('fs')
+var app = express()
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs')
+
+app.use((req, res, next) => {
+  let now = new Date().toString()
+  let log = `${now}: ${req.method} ${req.url}`
+  console.log(log)
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('unable to append to server.log')
+    }
+  })
+  next()
+})
+
+app.use((req, res, next) => {
+  res.render('maintenance.hbs', {
+    pageTitle: 'Site under maintenance',
+    currentYear: new Date().getFullYear(),
+    welcomeMessage: 'We will be back soon!'
+  })
+})
+
+app.use(express.static(__dirname + '/public'))
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+})
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase()
+})
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home',
+    welcome: 'Welcome to my home page',
+    currentYear: new Date().getFullYear()
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About page',
+    currentYear: new Date().getFullYear()
+  })
+})
+
+
+app.get('/bad', (req, res) => {
+  res.send({
+    error: 101,
+    messageMessage: 'Unable to handle request'
+  })
+})
+
+app.listen(3000, () => {
+  console.log('Server started on port: 3000')
+})
